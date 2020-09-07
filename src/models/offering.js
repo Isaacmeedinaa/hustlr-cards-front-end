ea7c232cd456eb89218ada76cc97c63bc7095cae
Isaac Medina <isaac.meedinaa@gmail.com>
2020-09-06 22:45:45 -0500

    fixed bugs, no end point needed

---
+++ src/models/offering.js
@@ -0,0 +1,11 @@
+class Offering {
+  constructor(id, title, description, offerType, cardId) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.offerType = offerType;
+    this.cardId = cardId;
+  }
+}
+
+export default Offering;
