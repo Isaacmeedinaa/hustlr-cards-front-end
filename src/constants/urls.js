4cbdd841ad366c5ee31aba2459ed1903dc5a013f
Isaac Medina <isaac.meedinaa@gmail.com>
2020-11-10 01:00:32 -0600

    added breakpoints; missing fix for zoom

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://hustlr.azurewebsites.net/api/v1' : 'http://localhost:5000/api/v1';
+export const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://hustlr.azurewebsites.net/api/v1"
+    : "http://localhost:5000/api/v1";
