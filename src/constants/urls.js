b1d9cb16b766c8247d87830c0d8946f1a4011526
Isaac Medina <isaac.meedinaa@gmail.com>
2020-11-25 22:03:49 -0600

    landing page fixes

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://hustlr.azurewebsites.net/api/v1"
-    : "http://localhost:5000/api/v1";
+    : "http://10.1.10.136:5000/api/v1";
