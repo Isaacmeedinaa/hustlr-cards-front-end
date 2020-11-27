90a0497c5f126b81fbc784d1966df2656781f491
Isaac Medina <isaac.meedinaa@gmail.com>
2020-11-27 16:34:28 -0600

    fixed offerings scroll

--- src/components/UI/publiccard/PublicCardOfferings.js
+++ src/components/UI/publiccard/PublicCardOfferings.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, createRef } from "react";
 
 import AwesomeSlider from "react-awesome-slider";
 
@@ -6,6 +6,28 @@ import "../../../constants/colors.css";
 import "./PublicCardUI.css";
 
 class PublicCardOfferings extends Component {
+  constructor() {
+    super();
+
+    this.offeringsContainer = createRef();
+  }
+
+  componentDidMount() {
+    this.offeringsContainer.current.addEventListener(
+      "wheel",
+      this.onOfferingsContainerWheel,
+      {
+        passive: false,
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    this.offeringsContainer.current.removeEventListener(
+      "wheel",
+      this.onOfferingsContainerWheel
+    );
+  }
 
   renderOfferingSliderImages = (offering) => {
     return offering.photos.map((photo) => (
@@ -15,7 +37,10 @@ class PublicCardOfferings extends Component {
 
   renderOfferings = () => {
     return this.props.offerings.map((offering) => {
-      if ((!offering.title || offering.title === "") && (!offering.description || offering.description === "")) {
+      if (
+        (!offering.title || offering.title === "") &&
+        (!offering.description || offering.description === "")
+      ) {
         return null;
       }
 
@@ -24,57 +49,77 @@ class PublicCardOfferings extends Component {
           key={offering.id}
           style={{ backgroundColor: this.props.transparentColor }}
           className="public-card-product-service-container"
-        > 
-          { offering.photos.length > 0 ? 
+        >
+          {offering.photos.length > 0 ? (
             //<img className="preview-card-offering-photo" src={offering.photos[0].url}/>
             <div className="slider-container">
               <AwesomeSlider bullets={false}>
-              {this.renderOfferingSliderImages(offering)}
-            </AwesomeSlider>
-            </div>
-            : null
-          }
-          <div className="public-card-offering-body">
-          <div className="public-card-product-service-header">
-            <p
-              style={{ color: this.props.primaryColor }}
-              className="public-card-product-service-title"
-            >
-              {offering.title}
-            </p>
-            <div className="public-card-product-service-price">
-              <span style={{ color: this.props.primaryColor }}>
-                {!offering.price ? null : "$" + offering.price}
-              </span>
-            </div>
-          </div>
-          {offering.description ? (
-            <div className="public-card-product-service-description word-wrap">
-              <span style={{ color: this.props.primaryColor }}>
-                {offering.description}
-              </span>
+                {this.renderOfferingSliderImages(offering)}
+              </AwesomeSlider>
             </div>
           ) : null}
+          <div className="public-card-offering-body">
+            <div className="public-card-product-service-header">
+              <p
+                style={{ color: this.props.primaryColor }}
+                className="public-card-product-service-title"
+              >
+                {offering.title}
+              </p>
+              <div className="public-card-product-service-price">
+                <span style={{ color: this.props.primaryColor }}>
+                  {!offering.price ? null : "$" + offering.price}
+                </span>
+              </div>
+            </div>
+            {offering.description ? (
+              <div className="public-card-product-service-description word-wrap">
+                <span style={{ color: this.props.primaryColor }}>
+                  {offering.description}
+                </span>
+              </div>
+            ) : null}
           </div>
         </div>
       );
     });
   };
 
-  render() {
-    if (this.props.offerings.length === 0) {
-      return null;
-    }
+  onOfferingsContainerWheel = (event) => {
+    event.preventDefault();
 
+    const container = document.getElementById(
+      "public-card-products-services-container"
+    );
+    const containerScrollPosition = document.getElementById(
+      "public-card-products-services-container"
+    ).scrollLeft;
+    container.scrollTo({
+      top: 0,
+      left: containerScrollPosition + event.deltaY,
+      behaviour: "smooth",
+    });
+  };
+
+  render() {
     return (
       <Fragment>
-        <div className="public-card-products-services-title-container">
-        <h4 className="ui horizontal divider header">
-          <span className="public-card-products-services-title-text">Products &amp; Services</span>
-        </h4>
-        </div>
-        <div className="public-card-products-services-container">
-          {this.renderOfferings()}
+        {this.props.offerings.length === 0 ? null : (
+          <div className="public-card-products-services-title-container">
+            <h4 className="ui horizontal divider header">
+              <span className="public-card-products-services-title-text">
+                Products &amp; Services
+              </span>
+            </h4>
+          </div>
+        )}
+        <div
+          className="public-card-products-services-container"
+          id="public-card-products-services-container"
+          onWheel={this.onOfferingsContainerWheel}
+          ref={this.offeringsContainer}
+        >
+          {this.props.offerings.length === 0 ? null : this.renderOfferings()}
         </div>
       </Fragment>
     );
